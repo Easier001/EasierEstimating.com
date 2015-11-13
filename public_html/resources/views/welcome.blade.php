091fd466bd067cace23d7bb215d6ca3c09e6a312
Easier001 <Snell@Geralds-MacBook-Pro.local>
2015-11-13 14:04:04 -0700

    Initial

---
+++ public_html/resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EasierEstimating.com</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Easier Estimating.com</div>
+            </div>
+        </div>
+        <div style='height:200px; margin-bottom:50px'>
+            <div style='margin:10px; background-image: url(http://localhost:8000/T5AM/Login.png); background-repeat: no-repeat; width:205px; height:180px; float:left'>
+            </div>
+
+            <div style='margin:10px; background-color: #315BD4; width:650px; height:180px; float:left'>
+
+            </div>
+
+        </div>
+        <div style='margin:10px; background-color: #315B94; width:1000px; height:50px'>
+
+        </div>
+        <div style='height:305px; margin-bottom:50px'>
+            <div style='margin:10px; background-image: url(http://localhost:8000/T5AM/MediaPlayer.png); background-repeat: no-repeat;  width:280px; height:255px; float:left'>
+            </div>
+            <div style='margin:10px; background-color: #315B94; width:500px; height:300px; float:left'>
+            </div>
+        </div>
+    </body>
+</html>
